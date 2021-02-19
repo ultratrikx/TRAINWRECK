1ca8a27c60664e5bf48e2af48d1d7ffe39d91ae1
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-02-19 11:50:27 -0500

    questions fix

--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 (function () {
+    // Functions
     function buildQuiz() {
         // variable to store the HTML output
         const output = [];
@@ -22,10 +23,12 @@
 
             // add this question and its answers to the output
             output.push(
-                `<div class="question"> ${
-                    currentQuestion.question
-                } </div>
-            <div class="answers"> ${answers.join('')} </div>`,
+                `<div class="slide">
+              <div class="question"> ${
+                  currentQuestion.question
+              } </div>
+              <div class="answers"> ${answers.join('')} </div>
+            </div>`,
             );
         });
 
@@ -71,6 +74,33 @@
         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
 
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
+        } else {
+            previousButton.style.display = 'inline-block';
+        }
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        } else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    // Variables
     const quizContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
@@ -110,6 +140,17 @@
     // Kick things off
     buildQuiz();
 
+    // Pagination
+    const previousButton = document.getElementById('previous');
+    const nextButton = document.getElementById('next');
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    // Show the first slide
+    showSlide(currentSlide);
+
     // Event listeners
     submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener('click', showPreviousSlide);
+    nextButton.addEventListener('click', showNextSlide);
 })();
