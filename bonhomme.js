3c201faef80485f1fc06fe3e79184e5e5f01b78b
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-02-19 11:50:57 -0500

    change

--- bonhomme.js
+++ bonhomme.js
@@ -37,39 +37,6 @@ bonhomme[0] = {
     y: 10 * box,
 };
 
-//adding questions
-const bonhommeQs = [
-    {
-        question: 'Who invented JavaScript?',
-        answers: {
-            a: 'Douglas Crockford',
-            b: 'Sheryl Sandberg',
-            c: 'Brendan Eich',
-        },
-        correctAnswer: 'c',
-    },
-    {
-        question:
-            'Which one of these is a JavaScript package manager?',
-        answers: {
-            a: 'Node.js',
-            b: 'TypeScript',
-            c: 'npm',
-        },
-        correctAnswer: 'c',
-    },
-    {
-        question: 'Which tool can you use to ensure code quality?',
-        answers: {
-            a: 'Angular',
-            b: 'jQuery',
-            c: 'RequireJS',
-            d: 'ESLint',
-        },
-        correctAnswer: 'd',
-    },
-];
-
 // create the food
 
 let food = {
