395ab4547ebb98adf774bd1299b46991b774c500
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-02-19 11:18:49 -0500

    thing

--- questionoverlay.js
+++
@@ -1,9 +0,0 @@
-import { bonhommeX, bonhommeY, food, food } from './bonhomme.js';
-
-if (bonhommeX == food.x && bonhommeY == food.y) {
-    document.getElementById('overlay').style.display = 'block';
-}
-
-function off() {
-    document.getElementById('overlay').style.display = 'none';
-}
